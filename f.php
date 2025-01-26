cfee5c6a9f7648fabcbc5510e638be079a5a5de0
Morigues <hid.ariako@gmail.com>
2025-01-26 07:09:52 +0700

    Update f.php

--- f.php
+++ f.php
@@ -3,42 +3,55 @@
 error_reporting(0);
 
 class Filter{	
-		public $email;
+    public $email;
+    public $password;
 
-		public function filtering(){
-			preg_match_all("/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/i", $this->email, $mailss);
-			return $mailss[0][0];
-		}
-		public function simpen($text, $nama){
-			$xyz = fopen($nama, "a+");
-				   fwrite($xyz, "$text\n");
-				   fclose($xyz);
-		}		
-		public function misahin(){
-			$filtering = $this->filtering();
-			if($filtering){
-				if (strstr($filtering, "@gmail") || strstr($filtering, "@googlemail")) {
-					$this->simpen($filtering, "gmail_family.txt");
-				}
-				elseif(strstr($filtering, "@hotmail") || strstr($filtering, "@msn") || strstr($filtering, "@outlook") || strstr($filtering, "@live")) {
-					$this->simpen($filtering, "hotmail_family.txt");
-				}
-				elseif(strstr($filtering, "@yahoo") || strstr($filtering, "@ymail")) {
-					$this->simpen($filtering, "yahoo_family.txt");
-				}
-				elseif(strstr($filtering, "@aol")){
-					$this->simpen($filtering, "aol_family.txt");
-				}
-				elseif(strstr($filtering, "@yandex")){
-					$this->simpen($filtering, "yandex_family.txt");
-				}
-				else {
-					$this->simpen($filtering, "other.txt");
-				}
-			}
-		}
+    public function filtering(){
+        preg_match_all("/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/i", $this->email, $mailss);
+        return $mailss[0][0];
+    }
+
+    public function passwordFilter($password){
+        // Example password filter: at least 8 characters, including at least one number and one letter
+        return preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $password);
+    }
+
+    public function simpen($text, $nama){
+        $xyz = fopen($nama, "a+");
+        fwrite($xyz, "$text\n");
+        fclose($xyz);
+    }		
+
+    public function misahin(){
+        $filtering = $this->filtering();
+        if($filtering){
+            if (strstr($filtering, "@gmail") || strstr($filtering, "@googlemail")) {
+                $this->simpen($filtering, "gmail_family.txt");
+            }
+            elseif(strstr($filtering, "@hotmail") || strstr($filtering, "@msn") || strstr($filtering, "@outlook") || strstr($filtering, "@live")) {
+                $this->simpen($filtering, "hotmail_family.txt");
+            }
+            elseif(strstr($filtering, "@yahoo") || strstr($filtering, "@ymail")) {
+                $this->simpen($filtering, "yahoo_family.txt");
+            }
+            elseif(strstr($filtering, "@aol")){
+                $this->simpen($filtering, "aol_family.txt");
+            }
+            elseif(strstr($filtering, "@yandex")){
+                $this->simpen($filtering, "yandex_family.txt");
+            }
+            else {
+                $this->simpen($filtering, "other.txt");
+            }
+
+            if ($this->passwordFilter($this->password)) {
+                $this->simpen($this->password, "valid_passwords.txt");
+            } else {
+                $this->simpen($this->password, "invalid_passwords.txt");
+            }
+        }
+    }
 }
-
 
 $ngefilter = new Filter();
 
@@ -52,8 +65,10 @@ $lines = count($list);
 echo "Please Wait... ";
 
 for ($i=0; $i<$lines; $i++) { 
-	$ngefilter->email = $list[$i];
-	$ngefilter->misahin();
+    $parts = explode(":", $list[$i]);
+    $ngefilter->email = $parts[0];
+    $ngefilter->password = isset($parts[1]) ? $parts[1] : '';
+    $ngefilter->misahin();
 }
 
 echo "DONE\n\n";
